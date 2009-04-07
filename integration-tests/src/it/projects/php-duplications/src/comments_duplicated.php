48e7159d0ec2bf870271c92ddb1ce202d0f8d38d
julien.lancelot <julien.lancelot@d25a0af3-c461-0410-a42d-853a01480fe4>
2009-04-07 13:05:28 +0000

    SONARPLUGINS-31 Scan PHP projects added duplcation sample tests
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@154 d25a0af3-c461-0410-a42d-853a01480fe4

---
+++ integration-tests/src/it/projects/php-duplications/src/comments_duplicated.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * A Class.
+ *
+ * @category   PHP
+ * @package    Sonar
+ * @copyright  Sonar
+ * @license    GNU Lesser General Public
+ * @version    Release: @package_version@
+ * @since      1.0
+ */
+class test1{
+
+}
+
+closedir($diary_directory);
+
+
+
+/**
+ * Another Class.
+ *
+ * @category   PHP
+ * @package    Sonar
+ * @copyright  Sonar
+ * @license    GNU Lesser General Public
+ * @version    Release: @package_version@
+ * @since      1.0
+ */
+class test2{
+
+}
+?>
