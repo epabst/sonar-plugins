c12d7b051a16871a95867d77888e77cc831879ab
simon.brandhof <simon.brandhof@d25a0af3-c461-0410-a42d-853a01480fe4>
2009-04-20 16:49:52 +0000

    add integration tests
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@243 d25a0af3-c461-0410-a42d-853a01480fe4

---
+++ integration-tests/src/it/projects/emma-execute-tests/src/test/java/org/sonar/plugins/emma/tests/HelloTest.java
@@ -0,0 +1,19 @@
+package org.sonar.plugins.emma.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+
+public class HelloTest {
+
+  @Test
+  public void shouldSayHello() {
+    assertEquals("hi", new Hello("hi").say());
+  }
+  
+  @Test
+  public void shouldNotFail() {
+    assertEquals(true, true);
+  }
+}
---
+++ integration-tests/src/it/projects/taglist/src/main/java/org/sonar/plugins/taglist/tests/Hello.java
@@ -0,0 +1,35 @@
+package org.sonar.plugins.taglist.tests;
+
+public class Hello {
+    private String hello;
+
+    public Hello(String s) {
+        //TODO
+        this.hello = s;
+    }
+
+    @Deprecated
+    public String say() {
+        return hello;
+    }
+
+    /**
+     * @deprecated
+     */
+    public void deprecatedMethod() {
+
+    }
+
+    /**
+     * @todo
+     */
+    public void todoMethod() {
+
+    }
+
+    protected int notCalled() {
+        int i = 0;
+        i++; // TODO
+        return i + 3;
+    }
+}
