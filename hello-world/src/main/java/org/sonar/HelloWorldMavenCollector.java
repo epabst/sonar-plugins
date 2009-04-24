0aa22951d138691d7bd52ad9d83047bce66bba96
simon.brandhof <simon.brandhof@d25a0af3-c461-0410-a42d-853a01480fe4>
2009-04-24 13:31:03 +0000

    move HelloWorld
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@252 d25a0af3-c461-0410-a42d-853a01480fe4

--- hello-world/src/main/java/org/sonar/HelloWorldMavenCollector.java
+++ hello-world/src/main/java/org/sonar/HelloWorldMavenCollector.java
@@ -1,4 +1,4 @@
-package org.codehaus.sonar;
+package org.sonar;
 
 import org.sonar.plugins.api.maven.MavenCollector;
 import org.sonar.plugins.api.maven.MavenPluginHandler;
---
+++ hello-world/src/main/java/org/sonar/HelloWorldMetrics.java
@@ -0,0 +1,27 @@
+package org.sonar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+import org.sonar.commons.Metric;
+import org.sonar.plugins.api.metrics.CoreMetrics;
+import org.sonar.plugins.api.metrics.Metrics;
+
+/**
+ * As the Metrics class implements org.sonar.plugins.api.Extension interface, 
+ * MyOwnMetrics is a Sonar extension which allows to declare as many new Metrics as you want.
+ */
+public class HelloWorldMetrics implements Metrics {
+
+  public static final Metric MESSAGE = new Metric("message_key", "Message",
+      "This is a metric to store a well known message", Metric.ValueType.STRING, -1, false,
+      CoreMetrics.DOMAIN_GENERAL, false);
+
+  // getMetrics() method is defined in the Metrics interface and is used by
+  // Sonar to retrieve the list of new Metric
+  public List<Metric> getMetrics() {
+    return Arrays.asList(MESSAGE);
+
+  }
+}
