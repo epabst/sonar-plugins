63273cd504e01da6677a9e56de4ffcf61981b40e
ogaudin <ogaudin@d25a0af3-c461-0410-a42d-853a01480fe4>
2009-07-24 08:07:57 +0000

    Adjust the copyright
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@455 d25a0af3-c461-0410-a42d-853a01480fe4

--- hammurapi/src/main/java/com/igate/plugins/hammurapi/HammurapiRulesRepository.java
+++ hammurapi/src/main/java/com/igate/plugins/hammurapi/HammurapiRulesRepository.java
@@ -5,15 +5,21 @@
  * Package        :   com.igate.plugins.hammurapi
  * Author         :   iGATE
  * Version        :   1.0
+ * Copyright (C) 2009 iGATE Corporation.
  *
- * Copyright:
- * This software is the confidential and proprietary information of iGATE Corporation.
- * ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use it only in accordance with the
- * terms of the license agreement you entered into with iGATE Corporation. You may not copy, adapt or
- * redistribute this document for commercial or non-commercial use or for your own
- * internal use without first obtaining express written approval from iGATE Corporation.
- * Copyright (C) 2009 iGATE Corporation. All rights reserved. Used by permission.
+ * Sonar Hammurapi Plugin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar Hammurapi Plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  *
  * Description:
  * This class sets up all the rules for the execution of the maven plugin.
