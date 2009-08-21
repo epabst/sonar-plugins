24d690580f1e05d7704a465bc3fe5b29495b02b4
freddy.mallet <freddy.mallet@d25a0af3-c461-0410-a42d-853a01480fe4>
2009-08-21 09:18:36 +0000

    suppress the public boolean shouldExecuteOnProject(Project project) method
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@562 d25a0af3-c461-0410-a42d-853a01480fe4

--- greenpepper/src/main/java/org/sonar/plugins/greenpepper/GreenPepperDecorator.java
+++ greenpepper/src/main/java/org/sonar/plugins/greenpepper/GreenPepperDecorator.java
@@ -26,23 +26,14 @@ import java.util.List;
 import org.sonar.api.batch.AbstractSumChildrenDecorator;
 import org.sonar.api.batch.DependedUpon;
 import org.sonar.api.measures.Metric;
-import org.sonar.api.resources.Java;
-import org.sonar.api.resources.Project;
 
 public class GreenPepperDecorator extends AbstractSumChildrenDecorator {
 
   @DependedUpon
   public List<Metric> generatesMetrics() {
     return Arrays.asList(GreenPepperMetrics.GREENPEPPER_SKIPPED_TESTS, GreenPepperMetrics.GREENPEPPER_TEST_ERRORS,
-        GreenPepperMetrics.GREENPEPPER_TEST_FAILURES,
-        GreenPepperMetrics.GREENPEPPER_TEST_SUCCESS_DENSITY, GreenPepperMetrics.GREENPEPPER_TESTS);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public boolean shouldExecuteOnProject(Project project) {
-    return project.getLanguage().equals(Java.INSTANCE);
+        GreenPepperMetrics.GREENPEPPER_TEST_FAILURES, GreenPepperMetrics.GREENPEPPER_TEST_SUCCESS_DENSITY,
+        GreenPepperMetrics.GREENPEPPER_TESTS);
   }
 
   @Override
