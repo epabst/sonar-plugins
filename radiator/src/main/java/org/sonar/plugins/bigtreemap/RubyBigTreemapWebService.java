7b9203b3591dc14bafbb87bd392d856f5d42cdab
simon.brandhof <simon.brandhof@d25a0af3-c461-0410-a42d-853a01480fe4>
2009-09-21 22:34:04 +0000

    radiator : add missing license header
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@698 d25a0af3-c461-0410-a42d-853a01480fe4

--- radiator/src/main/java/org/sonar/plugins/bigtreemap/RubyBigTreemapWebService.java
+++ radiator/src/main/java/org/sonar/plugins/bigtreemap/RubyBigTreemapWebService.java
@@ -1,3 +1,22 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Copyright (C) 2009 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
 package org.sonar.plugins.bigtreemap;
 
 import org.sonar.api.web.views.AbstractRubyTemplate;
@@ -8,7 +27,7 @@ public class RubyBigTreemapWebService extends AbstractRubyTemplate implements Ru
   public String getId() {
     return "RubyBigTreemapWebService";
   }
-  
+
   @Override
   protected String getTemplatePath() {
     return "/org/sonar/plugins/bigtreemap/big_treemap_plugin_controller.rb";
