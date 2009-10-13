0bee8634b20ecaeaaa44a41618ce97f0f42f480a
ogaudin <ogaudin@d25a0af3-c461-0410-a42d-853a01480fe4>
2009-10-13 21:37:55 +0000

    Add comments and unit test
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@808 d25a0af3-c461-0410-a42d-853a01480fe4

--- quality-index/src/main/java/org/sonar/plugins/qi/CodingViolationsDecorator.java
+++ quality-index/src/main/java/org/sonar/plugins/qi/CodingViolationsDecorator.java
@@ -6,25 +6,43 @@ import org.apache.commons.configuration.Configuration;
 
 public class CodingViolationsDecorator extends AbstractViolationsDecorator {
 
+  /**
+   * Creates a CodingViolationsDecorator, i.e. implements an AbstractViolationsDecorator
+   * to decorate the Coding axis of the QI
+   *
+   * @param configuration the configuration
+   */
   public CodingViolationsDecorator(Configuration configuration) {
     super(configuration, QIMetrics.QI_CODING_VIOLATIONS,
       QIPlugin.QI_CODING_AXIS_WEIGHT, QIPlugin.QI_CODING_AXIS_WEIGHT_DEFAULT);
   }
 
+  /**
+   * @return the coding axis weight config key
+   */
   @Override
   public String getConfigurationKey() {
     return QIPlugin.QI_CODING_PRIORITY_WEIGHTS;
   }
 
+  /**
+   * @return the coding axis default weight config key
+   */
   @Override
   public String getDefaultConfigurationKey() {
     return QIPlugin.QI_CODING_PRIORITY_WEIGHTS_DEFAULT;
   }
 
+  /**
+   * @return the metric to store the coding weighted violations
+   */
   public Metric getWeightedViolationMetricKey() {
     return QIMetrics.QI_CODING_WEIGHTED_VIOLATIONS;
   }
 
+  /**
+   * @return the PMD key
+   */
   @Override
   public String getPluginKey() {
     return CoreProperties.PMD_PLUGIN;
