e8c3b4984895130f1cf6884d491841690135aef1
hello2morrow <hello2morrow@d25a0af3-c461-0410-a42d-853a01480fe4>
2009-11-07 17:58:41 +0000

    Downgraded to JAXB 2.0
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@880 d25a0af3-c461-0410-a42d-853a01480fe4

--- sonarj/src/main/scripts/start.sh
+++ sonarj/src/main/scripts/start.sh
@@ -1,7 +1,7 @@
 sonardir=/opt/sonar-1.11
 arch=macosx-universal-32
-mvn package
+mvn clean install
 rm $sonardir/extensions/plugins/sonar-sonarj-plugin*
-cp target/sonar-sonarj-plugin-1.0-SNAPSHOT.jar $sonardir/extensions/plugins
+cp target/sonar-sonarj-plugin*.jar $sonardir/extensions/plugins
 $sonardir/bin/$arch/sonar.sh start
 
