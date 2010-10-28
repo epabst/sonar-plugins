e176b6f86259f9c315d9be9322d328948b74a254
godin <godin@d25a0af3-c461-0410-a42d-853a01480fe4>
2010-10-28 22:24:43 +0000

    SONARPLUGINS-774: Rename IT metrics
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@2482 d25a0af3-c461-0410-a42d-853a01480fe4

--- jacoco/src/main/java/org/sonar/plugins/jacoco/itcoverage/JaCoCoItMetrics.java
+++ jacoco/src/main/java/org/sonar/plugins/jacoco/itcoverage/JaCoCoItMetrics.java
@@ -34,43 +34,43 @@ import java.util.List;
  */
 public class JaCoCoItMetrics implements Metrics {
 
-  public static final String DOMAIN_IT_TESTS = "IT Tests";
+  public static final String DOMAIN_IT_TESTS = "Integration Tests";
 
   public static final String IT_COVERAGE_KEY = "it_coverage";
-  public static final Metric IT_COVERAGE = new Metric(IT_COVERAGE_KEY, "Coverage", "Coverage by unit tests", Metric.ValueType.PERCENT,
+  public static final Metric IT_COVERAGE = new Metric(IT_COVERAGE_KEY, "Coverage", "Coverage by integration tests", Metric.ValueType.PERCENT,
       Metric.DIRECTION_BETTER, true, DOMAIN_IT_TESTS).setWorstValue(0.0).setBestValue(100.0);
 
   public static final String IT_LINES_TO_COVER_KEY = "it_lines_to_cover";
-  public static final Metric IT_LINES_TO_COVER = new Metric(IT_LINES_TO_COVER_KEY, "Lines to cover", "Lines to cover", Metric.ValueType.INT,
+  public static final Metric IT_LINES_TO_COVER = new Metric(IT_LINES_TO_COVER_KEY, "IT Lines to cover", "IT Lines to cover", Metric.ValueType.INT,
       Metric.DIRECTION_BETTER, false, DOMAIN_IT_TESTS).setFormula(new SumChildValuesFormula(false)).setHidden(true);
 
   public static final String IT_UNCOVERED_LINES_KEY = "it_uncovered_lines";
-  public static final Metric IT_UNCOVERED_LINES = new Metric(IT_UNCOVERED_LINES_KEY, "Uncovered lines", "Uncovered lines", Metric.ValueType.INT,
+  public static final Metric IT_UNCOVERED_LINES = new Metric(IT_UNCOVERED_LINES_KEY, "IT Uncovered lines", "IT Uncovered lines", Metric.ValueType.INT,
       Metric.DIRECTION_WORST, false, DOMAIN_IT_TESTS).setFormula(new SumChildValuesFormula(false));
 
   public static final String IT_LINE_COVERAGE_KEY = "it_line_coverage";
-  public static final Metric IT_LINE_COVERAGE = new Metric(IT_LINE_COVERAGE_KEY, "Line coverage", "Line coverage", Metric.ValueType.PERCENT,
+  public static final Metric IT_LINE_COVERAGE = new Metric(IT_LINE_COVERAGE_KEY, "IT Line coverage", "IT Line coverage", Metric.ValueType.PERCENT,
       Metric.DIRECTION_BETTER, true, DOMAIN_IT_TESTS);
 
   public static final String IT_COVERAGE_LINE_HITS_DATA_KEY = "it_coverage_line_hits_data";
-  public static final Metric IT_COVERAGE_LINE_HITS_DATA = new Metric(IT_COVERAGE_LINE_HITS_DATA_KEY, "Coverage hits data", "Code coverage line hits data", Metric.ValueType.DATA,
+  public static final Metric IT_COVERAGE_LINE_HITS_DATA = new Metric(IT_COVERAGE_LINE_HITS_DATA_KEY, "IT Coverage hits data", "IT Code coverage line hits data", Metric.ValueType.DATA,
       Metric.DIRECTION_NONE, false, DOMAIN_IT_TESTS);
 
   public static final String IT_CONDITIONS_TO_COVER_KEY = "it_conditions_to_cover";
-  public static final Metric IT_CONDITIONS_TO_COVER = new Metric(IT_CONDITIONS_TO_COVER_KEY, "Conditions to cover", "Conditions to cover",
+  public static final Metric IT_CONDITIONS_TO_COVER = new Metric(IT_CONDITIONS_TO_COVER_KEY, "IT Conditions to cover", "IT Conditions to cover",
       Metric.ValueType.INT, Metric.DIRECTION_BETTER, false, DOMAIN_IT_TESTS).setFormula(new SumChildValuesFormula(false)).setHidden(true);
 
   public static final String IT_UNCOVERED_CONDITIONS_KEY = "it_uncovered_conditions";
-  public static final Metric IT_UNCOVERED_CONDITIONS = new Metric(IT_UNCOVERED_CONDITIONS_KEY, "Uncovered conditions", "Uncovered conditions",
+  public static final Metric IT_UNCOVERED_CONDITIONS = new Metric(IT_UNCOVERED_CONDITIONS_KEY, "IT Uncovered conditions", "IT Uncovered conditions",
       Metric.ValueType.INT, Metric.DIRECTION_WORST, false, DOMAIN_IT_TESTS).setFormula(new SumChildValuesFormula(false));
 
   public static final String IT_BRANCH_COVERAGE_KEY = "it_branch_coverage";
-  public static final Metric IT_BRANCH_COVERAGE = new Metric(IT_BRANCH_COVERAGE_KEY, "Branch coverage", "Branch coverage",
+  public static final Metric IT_BRANCH_COVERAGE = new Metric(IT_BRANCH_COVERAGE_KEY, "IT Branch coverage", "IT Branch coverage",
       Metric.ValueType.PERCENT, Metric.DIRECTION_BETTER, true, DOMAIN_IT_TESTS).setWorstValue(0.0).setBestValue(100.0);
 
   public static final String IT_BRANCH_COVERAGE_HITS_DATA_KEY = "it_branch_coverage_hits_data";
-  public static final Metric IT_BRANCH_COVERAGE_HITS_DATA = new Metric(IT_BRANCH_COVERAGE_HITS_DATA_KEY, "Branch coverage hits",
-      "Branch coverage hits", Metric.ValueType.DATA, Metric.DIRECTION_NONE, false, DOMAIN_IT_TESTS);
+  public static final Metric IT_BRANCH_COVERAGE_HITS_DATA = new Metric(IT_BRANCH_COVERAGE_HITS_DATA_KEY, "IT Branch coverage hits",
+      "IT Branch coverage hits", Metric.ValueType.DATA, Metric.DIRECTION_NONE, false, DOMAIN_IT_TESTS);
 
   public List<Metric> getMetrics() {
     return Arrays.asList(IT_COVERAGE, IT_LINES_TO_COVER, IT_UNCOVERED_LINES, IT_LINE_COVERAGE, IT_COVERAGE_LINE_HITS_DATA, IT_CONDITIONS_TO_COVER, IT_UNCOVERED_CONDITIONS, IT_BRANCH_COVERAGE, IT_BRANCH_COVERAGE_HITS_DATA);
