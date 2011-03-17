081c98bfb6c5e43c57e4c061e9772c187890040f
fabemn <fabemn@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-03-17 17:37:39 +0000

    [C#] Rename FxCop package to follow naming conventions
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@3507 d25a0af3-c461-0410-a42d-853a01480fe4

---
+++ csharp/sonar-csharp-fxcop-plugin/src/main/java/org/sonar/plugins/csharp/fxcop/FxCopConstants.java
@@ -0,0 +1,59 @@
+/*
+ * Sonar C# Plugin :: FxCop
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.plugins.csharp.fxcop;
+
+/**
+ * Constants of the FxCop plugin.
+ */
+public final class FxCopConstants {
+
+  private FxCopConstants() {
+  }
+
+  public static final String PLUGIN_KEY = "fxcop";
+  public static final String PLUGIN_NAME = "FxCop";
+
+  public static final String REPOSITORY_KEY = PLUGIN_KEY;
+  public static final String REPOSITORY_NAME = PLUGIN_NAME;
+
+  public static final String LANGUAGE_KEY = "cs";
+  public static final String LANGUAGE_NAME = "C#";
+
+  public static final String FXCOP_REPORT_XML = "fxcop-report.xml";
+  public static final String SL_FXCOP_REPORT_XML = "silverlight-fxcop-report.xml";
+
+  // ----------- Plugin Configuration Properties ----------- //
+  public static final String EXECUTABLE_KEY = "sonar.fxcop.executable";
+  public static final String EXECUTABLE_DEFVALUE = "C:/Program Files/Microsoft FxCop 10.0/FxCopCmd.exe";
+
+  public static final String ASSEMBLIES_TO_SCAN_KEY = "sonar.fxcop.assemblies";
+  public static final String ASSEMBLIES_TO_SCAN_DEFVALUE = "";
+
+  public static final String ASSEMBLY_DEPENDENCY_DIRECTORIES_KEY = "sonar.fxcop.assemblyDependencyDirectories";
+  public static final String ASSEMBLY_DEPENDENCY_DIRECTORIES_DEFVALUE = "";
+
+  public static final String IGNORE_GENERATED_CODE_KEY = "sonar.fxcop.ignoreGeneratedCode";
+  public static final boolean IGNORE_GENERATED_CODE_DEFVALUE = true;
+
+  public static final String TIMEOUT_MINUTES_KEY = "sonar.fxcop.timeoutMinutes";
+  public static final int TIMEOUT_MINUTES_DEFVALUE = 10;
+
+}
---
+++ csharp/sonar-csharp-fxcop-plugin/src/main/java/org/sonar/plugins/csharp/fxcop/FxCopPlugin.java
@@ -0,0 +1,105 @@
+/*
+ * Sonar C# Plugin :: FxCop
+ * Copyright (C) 2010 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
+package org.sonar.plugins.csharp.fxcop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.sonar.api.Extension;
+import org.sonar.api.Plugin;
+import org.sonar.api.Properties;
+import org.sonar.api.Property;
+import org.sonar.plugins.csharp.fxcop.profiles.FxCopProfileExporter;
+import org.sonar.plugins.csharp.fxcop.profiles.FxCopProfileImporter;
+import org.sonar.plugins.csharp.fxcop.profiles.SonarWay2Profile;
+import org.sonar.plugins.csharp.fxcop.profiles.SonarWayProfile;
+import org.sonar.plugins.csharp.fxcop.runner.FxCopRunner;
+
+
+/**
+ * Main class of the FxCop plugin.
+ */
+@Properties({
+    @Property(key = FxCopConstants.EXECUTABLE_KEY, defaultValue = FxCopConstants.EXECUTABLE_DEFVALUE, name = "FxCop executable",
+        description = "Absolute path of the FxCop program.", global = true, project = false),
+    @Property(key = FxCopConstants.ASSEMBLIES_TO_SCAN_KEY, defaultValue = FxCopConstants.ASSEMBLIES_TO_SCAN_DEFVALUE,
+        name = "Assemblies to scan", description = "Comma-seperated list of paths of assemblies that must be scanned.", global = false,
+        project = true),
+    @Property(key = FxCopConstants.ASSEMBLY_DEPENDENCY_DIRECTORIES_KEY,
+        defaultValue = FxCopConstants.ASSEMBLY_DEPENDENCY_DIRECTORIES_DEFVALUE, name = "Assembly dependency directories",
+        description = "Comma-seperated list of folders to search for assembly dependencies.", global = true, project = true),
+    @Property(key = FxCopConstants.IGNORE_GENERATED_CODE_KEY, defaultValue = FxCopConstants.IGNORE_GENERATED_CODE_DEFVALUE + "",
+        name = "Ignore generated code", description = "Suppress analysis results against generated code.", global = true, project = true),
+    @Property(key = FxCopConstants.TIMEOUT_MINUTES_KEY, defaultValue = FxCopConstants.TIMEOUT_MINUTES_DEFVALUE + "",
+        name = "FxCop program timeout", description = "Maximum number of minutes before the FxCop program will be stopped.", global = true,
+        project = true) })
+public class FxCopPlugin implements Plugin {
+
+  /**
+   * {@inheritDoc}
+   */
+  public String getKey() {
+    return FxCopConstants.PLUGIN_KEY;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public String getName() {
+    return FxCopConstants.PLUGIN_NAME;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public String getDescription() {
+    return "FxCop is an application that analyzes managed code assemblies (code that targets the .NET Framework common language runtime) "
+        + "and reports information about the assemblies, such as possible design, localization, performance, and security improvements. "
+        + "You can find more by going to the <a href='http://msdn.microsoft.com/en-us/library/bb429476(v=VS.80).aspx'>FxCop web site</a>.";
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public List<Class<? extends Extension>> getExtensions() {
+    List<Class<? extends Extension>> list = new ArrayList<Class<? extends Extension>>();
+    list.add(FxCopSensor.class);
+
+    // Rules and profiles
+    list.add(FxCopRuleRepository.class);
+    list.add(FxCopProfileImporter.class);
+    list.add(FxCopProfileExporter.class);
+    list.add(SonarWayProfile.class);
+    list.add(SonarWay2Profile.class);
+
+    // Running FxCop
+    list.add(FxCopRunner.class);
+    return list;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String toString() {
+    return getKey();
+  }
+}
