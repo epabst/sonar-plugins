028dfa68cd4d349d0ebe02db9352d44da8a463f2
godin <godin@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-04-05 16:02:10 +0000

    SONARPLUGINS-1078 Fix typo
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@3648 d25a0af3-c461-0410-a42d-853a01480fe4

--- c-rules/src/main/java/org/sonar/c/checks/SwitchStatementWithoutDefaultCheck.java
+++ c-rules/src/main/java/org/sonar/c/checks/SwitchStatementWithoutDefaultCheck.java
@@ -29,7 +29,7 @@ import com.sonar.c.api.CKeyword;
 import com.sonar.sslr.api.AstNode;
 import com.sonarsource.c.plugin.CCheck;
 
-@Rule(key = "C.SwitchStatementWithoutDefault", name = "Avoid swith statement without a \"default\" clause.",
+@Rule(key = "C.SwitchStatementWithoutDefault", name = "Avoid switch statement without a \"default\" clause.",
     isoCategory = IsoCategory.Reliability, priority = Priority.MAJOR,
     description = "<p>It's usually a good idea to introduce a default case in every switch statement. "
         + "Even if the developer is sure that all currently possible cases are covered, this should be expressed in the default branch. "
@@ -44,7 +44,7 @@ public class SwitchStatementWithoutDefaultCheck extends CCheck {
 
   public void visitNode(AstNode node) {
     if ( !node.hasChildren(CKeyword.DEFAULT)) {
-      log("Avoid swith statement without a \"default\" clause.", node);
+      log("Avoid switch statement without a \"default\" clause.", node);
     }
   }
 
