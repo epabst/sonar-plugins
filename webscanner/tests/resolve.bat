0b11ebeb2dc8adb246c84e924cbf61aeab9d7689
matthijs <matthijs@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-05-27 14:20:55 +0000

    WebScannerPlugin - remove from forge
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@3992 d25a0af3-c461-0410-a42d-853a01480fe4

--- webscanner/tests/resolve.bat
+++
@@ -1,1 +0,0 @@
-svn resolve --accept working -R .
--- webscanner/tests/validate-html.bat
+++
@@ -1,7 +0,0 @@
-
-rem run this script from webscanner folder
-
-set SONAR_FLAGS=-Dsonar.host.url=http://nlsivm114.sdmc.ao-srv.com:8080 -Dsonar.jdbc.url=jdbc:mysql://nlsivm114.sdmc.ao-srv.com:3306/sonar?useUnicode=true -Dsonar.jdbc.driver=com.mysql.jdbc.Driver
-set DEBUG= 
-
-call mvn sonar:sonar -X -f html-pom.xml %SONAR_FLAGS% %DEBUG% > sonar-html.log
