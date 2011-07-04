56be91711f47305384ceacdcf20f9ca533404203
rsantamaria <rsantamaria@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-07-04 07:57:12 +0000

    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@4136 d25a0af3-c461-0410-a42d-853a01480fe4

---
+++ c-rules/src/test/resources/checks/BreakCheck.java
@@ -0,0 +1,30 @@
+package org.sonar.c.checks;
+
+import org.sonar.check.BelongsToProfile;
+import org.sonar.check.IsoCategory;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+
+import com.sonar.c.api.CKeyword;
+import com.sonar.sslr.api.AstNode;
+import com.sonarsource.c.plugin.CCheck;
+
+@Rule(key = "C.DoNotUseBreak", name = "Break statement must be used only inside a switch block", isoCategory = IsoCategory.Usability, priority = Priority.MAJOR,
+	    description = "<p>Avoid using keyword break outside a switch block.</p>")
+@BelongsToProfile(title = CChecksConstants.SONAR_C_WAY_PROFILE_KEY, priority = Priority.MAJOR)
+
+public class BreakCheck extends CCheck
+{
+    @Override
+    public void init() {
+	subscribeTo(getCGrammar().jumpStatement);
+    }
+
+    public void visitNode(AstNode node)
+    {
+	if((!node.hasParents(getCGrammar().switchStatement)) && 
+		(node.hasChildren(CKeyword.BREAK))) {
+	    log("Keyword break only allowed inside a switch block.", node);
+	}
+    }
+}
