303fa7bd6b07849e448b2a7c1d5fb098cfcbe418
freddy.mallet <freddy.mallet@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-08-26 14:10:45 +0000

    SONARPLUGINS-1228 Break Check
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@4384 d25a0af3-c461-0410-a42d-853a01480fe4

--- c-rules/src/main/java/org/sonar/c/checks/BreakCheck.java
+++ c-rules/src/main/java/org/sonar/c/checks/BreakCheck.java
@@ -17,7 +17,7 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
- 
+
 package org.sonar.c.checks;
 
 import org.sonar.check.BelongsToProfile;
@@ -29,22 +29,19 @@ import com.sonar.c.api.CKeyword;
 import com.sonar.sslr.api.AstNode;
 import com.sonarsource.c.plugin.CCheck;
 
-@Rule(key = "C.DoNotUseBreak", name = "Break statement must be used only inside a switch block", isoCategory = IsoCategory.Usability, priority = Priority.MAJOR,
-	    description = "<p>Avoid using keyword break outside a switch block.</p>")
-//@BelongsToProfile(title = CChecksConstants.SONAR_C_WAY_PROFILE_KEY, priority = Priority.MAJOR)
+@Rule(key = "C.DoNotUseBreak", name = "Break statement must be used only inside a switch block", isoCategory = IsoCategory.Usability,
+    priority = Priority.MAJOR, description = "<p>Avoid using keyword break outside a switch block.</p>")
+@BelongsToProfile(title = CChecksConstants.SONAR_C_WAY_PROFILE_KEY, priority = Priority.MAJOR)
+public class BreakCheck extends CCheck {
 
-public class BreakCheck extends CCheck
-{
-    @Override
-    public void init() {
-        subscribeTo(getCGrammar().jumpStatement);
-    }
+  @Override
+  public void init() {
+    subscribeTo(getCGrammar().jumpStatement);
+  }
 
-    public void visitNode(AstNode node)
-    {
-        if((!node.hasParents(getCGrammar().switchStatement)) && 
-            (node.hasChildren(CKeyword.BREAK))) {
-            log("Keyword break only allowed inside a switch block.", node);
-        }
+  public void visitNode(AstNode node) {
+    if (( !node.hasParents(getCGrammar().switchStatement)) && (node.hasChildren(CKeyword.BREAK))) {
+      log("Keyword break only allowed inside a switch block.", node);
     }
+  }
 }
---
+++ c-rules/src/test/resources/checks/break.c
@@ -0,0 +1,11 @@
+
+
+static void main()
+{
+   break;
+    switch(c){
+      case '0' : 
+        ++nespace;
+        break;
+    }
+}
