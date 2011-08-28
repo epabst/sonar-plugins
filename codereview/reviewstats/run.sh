c0f5c60083921f6dba108ffcf9df45ca1a2bb3cd
ogaudin <ogaudin@d25a0af3-c461-0410-a42d-853a01480fe4>
2011-08-28 09:00:31 +0000

    remove codereview project
    git-svn-id: https://svn.codehaus.org/sonar-plugins/trunk@4387 d25a0af3-c461-0410-a42d-853a01480fe4

--- codereview/reviewstats/run.sh
+++
@@ -1,3 +0,0 @@
-cd target
-cd classes
-java -cp .:/mnt/share/sync/MavenRepo/Repo1/commons-io/commons-io/1.4/commons-io-1.4.jar:/mnt/share/sync/MavenRepo/Repo1/com/thoughtworks/xstream/xstream/1.3.1/xstream-1.3.1.jar   com.symcor.sonar.reviewstats.CodeReviewStats ~/code/dev/sonar/test-project-sonar ~/code/dev/sonar/reviewstats/target
--- codereview/sonar-plugin-reviewinfo/run.sh
+++
@@ -1,9 +0,0 @@
-cd target/classes
-
-java -cp .:/mnt/share/sync/MavenRepo/Repo1/com/thoughtworks/xstream/xstream/1.3.1/xstream-1.3.1.jar:\
-/mnt/share/sync/MavenRepo/Repo1/org/codehaus/sonar/sonar-plugin-api/1.10.1/sonar-plugin-api-1.10.1.jar:\
-/mnt/share/sync/MavenRepo/Repo1/commons-lang/commons-lang/2.4/commons-lang-2.4.jar:\
-/mnt/share/sync/MavenRepo/Repo1/commons-io/commons-io/1.4/commons-io-1.4.jar:\
-/mnt/share/sync/MavenRepo/Repo1/com/symcor/sonar/reviewstats/1.0/reviewstats-1.0.jar  \
-     org.sonar.plugins.svninfo.ReviewStatsXMLHandler
-#     org.sonar.plugins.svninfo.SvnInfoXMLHandler
